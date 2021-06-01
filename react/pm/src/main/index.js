921ae50a2c138217b48bb21f40a7f5e872c44b17
rlaehdud1012 <66362265+rlaehdud1012@users.noreply.github.com>
2021-06-01 13:30:23 +0900

    recent

--- react/pm/src/main/index.js
+++ react/pm/src/main/index.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 import "./index.css";
 class MainPage extends Component {
   render() {
-    return <div>main</div>;
+    return (
+      <div>
+        <img src="./diet.jpg" alt="메인이미지" />
+      </div>
+    );
   }
 }
 
