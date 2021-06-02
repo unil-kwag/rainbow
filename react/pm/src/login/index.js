7455e2fef1639b467187dc4d06b54b4a9c281f69
rlaehdud1012 <66362265+rlaehdud1012@users.noreply.github.com>
2021-06-02 11:01:46 +0900

    react

--- react/pm/src/login/index.js
+++ react/pm/src/login/index.js
@@ -1,24 +1,39 @@
 import React from "react";
 import "./index.css";
+import { Form, Button } from "antd";
 
 function LoginPage() {
   return (
-    <form id="login">
-      <p>
-        username
-        <input class="userbox" type="text" name="userid" />
-      </p>
-      <p>
-        password
-        <input class="userbox" name="userpassword" />
-      </p>
-      <div id="box">
-        <input type="button" position value="login" />
-        <input type="button" onclick="" value="join" />
-        <input type="reset" value="reset" />
-      </div>
-      {/* <script src="login.js"></script> */}
-    </form>
+    <div id="Lp">
+      <form id="login">
+        <div id="idname">로그인</div>
+        <div id="id">
+          <input
+            class="userbox"
+            type="text"
+            name="userid"
+            placeholder="아이디를 입력하세요"
+          />
+        </div>
+        <div id="password">
+          <input
+            class="pw"
+            type="password"
+            name="userpassword"
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+        <div id="box">
+          <Button id="loginbox" type="primary">
+            로그인
+          </Button>
+          <Button id="pwbox" type="primary">
+            회원가입
+          </Button>
+        </div>
+        {/* <script src="login.js"></script> */}
+      </form>
+    </div>
   );
 }
 
